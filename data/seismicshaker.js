d26914f9868686a1503f0643a42af672a584a1e4
mackers <me@mackers.com>
2011-12-08 17:07:54 +0100

    map interaction improvements. page changes.

--- data/seismicshaker.js
+++ data/seismicshaker.js
@@ -17,12 +17,51 @@ self.port.on('mylocation', function(mylocation)
     if (!map)
         return;
 
+    if (unsafeWindow.currentPopup)
+        unsafeWindow.currentPopup.hide();
+
     mylocationmarker.clearMarkers();
 
+    /*
     var size = new OpenLayers.Size(21,25);
     var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
     var icon = new OpenLayers.Icon('img/marker-blue.png', size, offset);
     var marker = new OpenLayers.Marker(new OpenLayers.LonLat(mylocation.lng, mylocation.lat),icon);
+
+    mylocationmarker.addMarker(marker);
+    */
+
+        var feature = new OpenLayers.Feature(mylocationmarker, new OpenLayers.LonLat(mylocation.lng, mylocation.lat)); 
+        feature.closeBox = true;
+        feature.popupClass = OpenLayers.Class(OpenLayers.Popup.AnchoredBubble, {'autoSize': true});
+        feature.data.popupContentHTML = "You are here.";
+        feature.data.overflow = "auto";
+                
+        var marker = feature.createMarker();
+
+/*
+        marker.size = new OpenLayers.Size(21,25);
+        marker.offset = new OpenLayers.Pixel(-(marker.size.w/2), -marker.size.h);
+        marker.icon = new OpenLayers.Icon('img/marker-blue.png', marker.size, marker.offset);
+        */
+
+        marker.icon.url = "img/marker-blue.png";
+
+        var markerClick = function (evt) {
+            if (unsafeWindow.currentPopup)
+                unsafeWindow.currentPopup.hide();
+
+            if (this.popup == null) {
+                this.popup = this.createPopup(true);
+                map.addPopup(this.popup);
+                this.popup.show();
+            } else {
+                this.popup.toggle();
+            }
+            unsafeWindow.currentPopup = this.popup;
+            OpenLayers.Event.stop(evt);
+        };
+        marker.events.register("click", feature, markerClick);
 
     mylocationmarker.addMarker(marker);
 
@@ -34,14 +73,11 @@ self.port.on('quakes', function(quakes)
     var map = unsafeWindow.map;
     var markers = unsafeWindow.markers;
     var OpenLayers = unsafeWindow.OpenLayers;
-    var currentPopup;
 
     if (!map)
         return;
 
-    AutoSizeAnchoredBubble = OpenLayers.Class(OpenLayers.Popup.AnchoredBubble, {
-        'autoSize': true
-        });
+    AutoSizeAnchoredBubble = OpenLayers.Class(OpenLayers.Popup.AnchoredBubble, {'autoSize': true});
 
     markers.clearMarkers();
 
@@ -56,8 +92,8 @@ self.port.on('quakes', function(quakes)
         var marker = feature.createMarker();
 
         var markerClick = function (evt) {
-            if (currentPopup)
-                currentPopup.hide();
+            if (unsafeWindow.currentPopup)
+                unsafeWindow.currentPopup.hide();
 
             if (this.popup == null) {
                 this.popup = this.createPopup(this.closeBox);
@@ -66,8 +102,8 @@ self.port.on('quakes', function(quakes)
             } else {
                 this.popup.toggle();
             }
-            currentPopup = this.popup;
-            //OpenLayers.Event.stop(evt);
+            unsafeWindow.currentPopup = this.popup;
+            OpenLayers.Event.stop(evt);
         };
         marker.events.register("click", feature, markerClick);
 
