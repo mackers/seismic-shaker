950040d272395fed1fb84a71b4b08e0474a67831
David McNamara <me@mackers.com>
2012-05-28 17:52:03 +0200

    2.0.2 built with sdk 1.7. updated license to mpl 2

--- data/shake.js
+++ data/shake.js
@@ -1,3 +1,7 @@
+/*This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/.*/
+
 self.port.on('shake', function()
 {
     for (let i=10; i>0; i--)
